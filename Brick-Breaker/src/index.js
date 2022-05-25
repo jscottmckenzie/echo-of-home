6baae057b323a7c423daa61a79422b949c6dafc0
jscottmckenzie <jscottmckenzie@gmail.com>
2022-05-25 14:23:37 -0400

    do it

--- Brick-Breaker/src/index.js
+++ Brick-Breaker/src/index.js
@@ -1,4 +1,20 @@
-import Paddle from './paddle.js';
+class Paddle {
+
+    constructor(gameWidth, gameHeight) {
+        this.width = 150;
+        this.height = 20;
+        this.position = {
+            x: gameWidth / 2 - this.width / 2,
+            y: gameHeight - this.height - 10,
+        }
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = '#0ff';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+}
 
 let canvas = document.getElementById('gameScreen');
 let ctx = canvas.getContext('2d');
